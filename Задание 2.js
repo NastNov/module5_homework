6d9713b8183546ac1bc152ccd113c68e14117e1d
NastNov <111925797+NastNov@users.noreply.github.com>
2022-09-20 00:14:33 +0300

    Create Задание 2.js

---
+++ Задание 2.js
@@ -0,0 +1,12 @@
+// Задание 2
+
+let x = 1;
+if (typeof x === 'number') {
+    console.log ("х = числу");
+} else if (typeof x === 'string') {
+    console.log ("х = строке");
+} else if (typeof x === 'boolean') {
+    console.log ("х = логическому типу");
+} else {
+    console.log ("Тип x не определён");
+}
