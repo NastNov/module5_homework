e8af81b24b16fa542ac2430cd419a20be749d412
NastNov <111925797+NastNov@users.noreply.github.com>
2022-09-20 00:15:31 +0300

    Create Задание 4.js

---
+++ Задание 4.js
@@ -0,0 +1,10 @@
+// Задание 4
+
+let min = 0;
+let max = 100;
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log (getRandomIntInclusive (min, max));
