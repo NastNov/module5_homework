eb7bdcfcfa8800d6a05d3e1715e9f373b08c7394
NastNov <111925797+NastNov@users.noreply.github.com>
2022-09-20 00:17:16 +0300

    Create Задание 7.js

---
+++ Задание 7.js
@@ -0,0 +1,11 @@
+// Задание 7
+
+let arr = [1, 2, 3, 4, 5];
+let result = arr.filter(function(elem) {
+	if (elem % 2 == 0) {
+		return true;
+	} else {
+		return false;
+	}
+});
+console.log (result.length)
