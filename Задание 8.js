3611ce4bebd854f74f9d1d36bf7c68be15c7e2a8
NastNov <111925797+NastNov@users.noreply.github.com>
2022-09-20 00:17:41 +0300

    Create Задание 8.js

---
+++ Задание 8.js
@@ -0,0 +1,16 @@
+// Задание 8
+
+let fruits = new Map([
+
+    ["apple", "green"],
+  
+    ["strawberry", "red"],
+  
+    ["blueberry", "blue"]
+  
+  ]);
+  for (let elem of fruits) { 
+  
+     console.log(elem);
+  
+  }
